f60c91108dbf57afa0011138ddab541d9d611d32
Abhilipsa13 <jenaabhilipsa27@gmail.com>
2025-02-21 09:31:25 +0530

    Ternary Operations

---
+++ Ternary.java
@@ -0,0 +1,7 @@
+public class Ternary{
+    public static void main(String[] args) {
+        int x =10;
+        String val = x>10?"x is greater":"x is not greater";
+        System.out.println(val);
+    }
+}
